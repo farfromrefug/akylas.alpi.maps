618acf04b61de867bb3f27fb0bd7286c54977e09
Martin Guillon <martin.guillon@akylas.fr>
2018-05-11 17:23:44 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,111 @@
-const path = require("path");
-const webpack = require("webpack");
-const glob = require("glob");
-const _ = require('lodash');
-const {
-    TsConfigPathsPlugin,
-    CheckerPlugin
-} = require('awesome-typescript-loader');
-const Resources = path.resolve(__dirname, "Resources");
-const nodejs = "/Volumes/data/dev/titanium/modules/akylas_modules/nodejs/Resources";
-const test  = Object.assign({},
-    _.reduce(glob.sync(path.join(Resources, "ui", "*.ts")),
-        (obj, val) => {
-            val = path.relative(Resources, val);
-            obj[val.split('.').slice(0, -1).join('.')] = "./" + val;
-            return obj;
-        }, {}), {
-        app: "./app.ts",
-        // vendor: [
-            // 'lodash'
-        // ]
-    }
-)
-console.log('test', test);
-module.exports = {
-    // entry: "./app.ts",
-    entry: test,
-    context: Resources,
-    output: {
-        libraryTarget: "commonjs",
-        pathinfo: true,
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
-    devtool: "sourcemap",
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".js", ".json"],
-        modules: [
-            "/Volumes/data/dev/titanium/modules/akylas_modules/nodejs/Resources",
-            path.join(Resources, "node_modules")
-          ],
-          alias:{
-              "process": path.resolve(nodejs, "process.js"),
-              "buffer": path.resolve(nodejs, "buffer.ts"),
-              "events": path.resolve(nodejs, "events.js"),
-              "path": path.resolve(nodejs, "path.js"),
-              "os": path.resolve(nodejs, "os.ts"),
-              "fs": path.resolve(nodejs, "fs.ts"),
-              "inherits": path.resolve(nodejs, "inherits.js"),
-              "utils": path.resolve(nodejs, "utils.ts"),
-          }
-    },
-    externals: [
-        /^akylas\.commonjs.*$/
-    ],
-    target:() => undefined,
-    // Add the loader for .ts files.
-    module: {
-        rules: [{
-            test: /\.(ts)$/,
-            loader: 'awesome-typescript-loader',
-             exclude: [/locale/],
-            include: [
-                path.resolve(__dirname, "Resources")
+const path = require('path');
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+
+function titaniumTarget(compiler) {
+    var options = this;
+    var webpackLib = 'webpack/lib';
+
+    var JsonpTemplatePlugin = require(webpackLib + '/web/JsonpTemplatePlugin');
+    var FunctionModulePlugin = require(webpackLib + '/FunctionModulePlugin');
+    var LoaderTargetPlugin = require(webpackLib + '/LoaderTargetPlugin');
+
+    compiler.apply(new JsonpTemplatePlugin(options.output), new FunctionModulePlugin(options.output), new LoaderTargetPlugin('web'));
+}
+
+function IgnoreUnresolvedPlugin() {}
+
+IgnoreUnresolvedPlugin.prototype.apply = function(compiler) {
+    compiler.plugin('compilation', function(compilation, data) {
+        data.normalModuleFactory.plugin('parser', function(parser) {
+            parser.plugin('call require', function(params) {
+                if (params.arguments.length !== 1) {
+                    return;
+                }
+
+                const param = this.evaluateExpression(params.arguments[0]);
+
+                if (!param.isString() && !param.isConditional()) {
+                    return true;
+                }
+            });
+        });
+    });
+};
+
+module.exports = env => {
+    const Resources = path.resolve(__dirname, 'Resources');
+    const platform = env.platform;
+    const platformBuildDir = platform.replace('ios', 'iphone');
+    const target = env.target;
+    const deployType = env.deployType;
+    const nodejs = path.join(__dirname, 'plugins/akylas.nodejs/1.0.0/Resources');
+    const tsConfigFile = path.join(__dirname, 'tsconfig.json');
+    const config = {
+        mode: 'none',
+        stats: 'minimal',
+        performance: {
+            hints: false
+        },
+        entry: './app.ts',
+        context: Resources,
+        output: {
+            libraryTarget: 'commonjs',
+            libraryExport: 'default',
+            pathinfo: true,
+            filename: 'app.js',
+            path: __dirname + '/dist'
+        },
+        // devtool: 'sourcemap',
+        resolve: {
+            extensions: ['.ts', '.js', '.json'],
+            plugins:[new TsConfigPathsPlugin({ configFile: tsConfigFile })],
+            modules: [Resources, nodejs, path.join(Resources, 'node_modules'), path.join(__dirname, 'build', platformBuildDir, 'rjss')]
+        },
+        target: titaniumTarget,
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    options: {
+                        configFile: tsConfigFile,
+                        transpileOnly: true
+                    }
+                }
             ]
-        // }, {
-        //     test: /\.(ts)$/,
-        //     loader: 'awesome-typescript-loader',
-        //     include: [
-        //         "/Volumes/data/dev/titanium/modules/akylas_modules/nodejs/Resources"
-        //     ]
-        }],
-    },
-    plugins: [
-        new TsConfigPathsPlugin( /* { configFileName, compiler } */ ),
-        new CheckerPlugin(),
-        // new webpack.LoaderOptionsPlugin({
-        //     debug: true
-        // }),
-        // new webpack.DefinePlugin({
-        //     DEBUG: true,
-        //     PRODUCTION: false
-        // })
-    ]
+        },
+        // externals: ['akylas.statusbarnotification', 'akylas.zoomableimage'],
+        plugins: [
+            new IgnoreUnresolvedPlugin(),
+            new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr/),
+           
+            // new BundleAnalyzerPlugin({
+            //     analyzerMode: 'static',
+            //     openAnalyzer: false,
+            //     generateStatsFile: true,
+            //     reportFilename: path.join(__dirname, 'report', `report.html`),
+            //     statsFilename: path.join(__dirname, 'report', `stats.json`)
+            // })
+        ]
+    };
+    if (deployType === 'development') {
+        config.plugins.unshift(
+            new webpack.IgnorePlugin(/rjss\.compiled/)
+        );
+    } else {
+        config.plugins.push( new UglifyJsPlugin({
+            uglifyOptions: {
+                mangle:true,
+                output: {
+                    beautify: false
+                },
+                keep_classnames: true
+            }
+        }));
+    }
+    return config;
 };
